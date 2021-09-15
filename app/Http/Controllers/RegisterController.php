b9ddd35a6f4a483cb922caf386306c3c656ee73f
Ricky <anandaricky01@gmail.com>
2021-09-15 22:10:49 +0700

    menambah user registration

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class RegisterController extends Controller
 {
@@ -13,7 +15,21 @@ class RegisterController extends Controller
         ]);
     }
 
-    public function store(){
-        return request()->all;
+    public function store(Request $request){
+        $validatedData = $request->validate([
+            'name' => 'required|max:255', // bisa pakai pipe
+            'username' => ['required', 'min:4', 'max:255', 'unique:users'], // bisa pake array formating
+            'email' => 'required|email:dns|unique:users',
+            'password' => 'required|min:5|max:255'
+        ]);
+
+        // $validatedData['password'] = bcrypt($validatedData['password']);
+        $validatedData['password'] = Hash::make($validatedData['password']);
+
+        User::create($validatedData);
+
+        $request->session()->flash('success', 'Pendaftaran Berhasil!');
+
+        return redirect('/login');
     }
 }
