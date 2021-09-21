e1256b4e9908dd48a5bd5bc2c6902915425cc638
Ricky <anandaricky01@gmail.com>
2021-09-21 13:52:47 +0700

    menambah fitur login dan middleware +dashboard

--- resources/views/login/index.blade.php
+++ resources/views/login/index.blade.php
@@ -12,17 +12,29 @@
         </div>    
       @endif
 
+      @if(session()->has('loginError'))
+        <div class="alert alert-danger alert-dismissible fade show" role="alert">
+          <p>{{ session('loginError') }}</p>
+          <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+        </div>    
+      @endif
+
       <main class="form-signin">
           <h1 class="h3 mb-3 fw-normal text-center">Halaman Log in</h1>
-          <form>
-        
+          <form action="/login" method="post">
+            @csrf
             <div class="form-floating">
-              <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
-              <label for="floatingInput">Email address</label>
+              <input type="email" class="form-control @error('email') is-invalid @enderror" id="email" placeholder="name@example.com" name="email" autofocus required value="{{ old('email') }}">
+              <label for="email">Email address</label>
+              @error('email')
+                  <div class="invalid-feedback">
+                    {{ $message }}
+                  </div>
+              @enderror
             </div>
             <div class="form-floating">
-              <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
-              <label for="floatingPassword">Password</label>
+              <input type="password" class="form-control" id="password" placeholder="Password" name="password" required>
+              <label for="password">Password</label>
             </div>
         
             <button class="w-100 btn btn-lg btn-primary" type="submit">Log in</button>
