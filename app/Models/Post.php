dcb726c8b3f1d1f725fc6888b4e26cac7b31febc
Ricky <anandaricky01@gmail.com>
2021-09-27 18:53:44 +0700

    Menambah create post form untuk new post

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,10 +4,11 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Cviebrock\EloquentSluggable\Sluggable;
 
 class Post extends Model
 {
-    use HasFactory;
+    use HasFactory, Sluggable;
 
     // 
     // protected $fillable = ['judul','excerpt','body'];
@@ -76,6 +77,15 @@ class Post extends Model
     public function getRouteKeyName()
     {
         return 'slug';
+    }
+
+    public function sluggable(): array
+    {
+        return [
+            'slug' => [
+                'source' => 'judul'
+            ]
+        ];
     }
 }
 
---
+++ resources/views/dashboard/posts/create.blade.php
@@ -0,0 +1,53 @@
+@extends('dashboard/layouts/main')
+
+@section('container')
+<div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+    <h1 class="h2">Create new Posts</h1>
+</div>
+
+<div class="col-lg-8">
+    <form action="/dashboard/posts" method="POST">
+        @csrf
+        <div class="mb-3">
+          <label for="judul" class="form-label">Judul</label>
+          <input type="text" class="form-control" id="judul" name="judul">
+        </div>
+        <div class="mb-3">
+          <label for="slug" class="form-label">Slug</label>
+          <input type="text" class="form-control" id="slug" name="slug">
+        </div>
+        <div class="mb-3">
+          <label for="category" class="form-label">Category</label>
+          <select class="form-select" name="category_id">
+            @foreach ($categories as $category)
+              <option value="{{ $category->id }}">{{ $category->name }}</option>
+            @endforeach
+          </select>
+        </div>
+        <div class="mb-3">
+          <label for="body" class="form-label">Body</label>
+          <input id="body" type="hidden" name="body">
+          <trix-editor input="body"></trix-editor>
+        </div>
+        <button type="submit" class="btn btn-primary">Create Post</button>
+    </form>
+</div>
+
+
+<script>
+  const judul = document.querySelector('#judul');
+  const slug = document.querySelector('#slug');
+
+  judul.addEventListener('change', function () {
+    fetch('/dashboard/posts/checkSlug?judul=' + judul.value)
+      .then(response => response.json())
+      .then(data => slug.value = data.slug)
+
+  });
+
+  document.addEventListener('trix-file-accept', function(e){
+    e.preventDefault();
+  });
+</script>
+    
+@endsection
